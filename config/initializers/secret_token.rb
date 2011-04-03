267ed27d994922ff89c5125507f781d07ef47ef6
root <root@lizbeth-desktop.(none)>
2011-04-03 16:34:56 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '691959461e28d1483083185879555b4e06a4960f53fae24013ed22c46509f6cbe773bf623382c742a5669db15007d819797310cbb91aeb483ea41e3ae02d48ad'
